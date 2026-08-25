514b7ec6ef43c5ed7390e362c3dc5f855a0c0989
David Ranzolin <daranzolin@gmail.com>
2026-08-25 14:37:54 -0700

    Update upload_report_to_sharepoint.R

--- reports/upload_report_to_sharepoint.R
+++ reports/upload_report_to_sharepoint.R
@@ -1,7 +1,7 @@
 library(Microsoft365R)
 
 file_to_upload <- Sys.getenv("QUARTO_PROJECT_OUTPUT_FILES")
-new_sp_reports_dir <- targets::tar_read(wfc_monthly_reports_dir, store = here::here("_targets"))
+new_sp_reports_dir <- targets::tar_read(sharepoint_monthly_reports_dir, store = here::here("_targets"))
 sp <- get_sharepoint_site("SHAREPOINT_SITE")
 sp_docs <- sp$get_drive("Documents")
 
